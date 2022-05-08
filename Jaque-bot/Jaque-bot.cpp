de1afadbc117ca32305b3a6dcdc2fe3214a16d11
Jorge <j.almoyna@alumnos.upm.es>
2022-05-08 12:59:00 +0200

    una cosa q se me olvidó xd

--- Jaque-bot/Jaque-bot.cpp
+++ Jaque-bot/Jaque-bot.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include "ETSIDI.h"
 #include "freeglut.h"
+#include "Pieza.h"
+#include "Peon.h"
+#include "Reina.h"
+#include "Alfil.h"
+#include "Rey.h"
+#include "Caballo.h"
+#include "Torre.h"
+
+using namespace ETSIDI;
+
+void inicio();
 
 int main()
 {
-    bool valor = ETSIDI::lanzaMoneda();
-    std::cout << ("%c\n", valor);
+	inicio();
 }
+
+void inicio() {}
