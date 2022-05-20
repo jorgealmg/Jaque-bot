a10b4837f11411e2c33e2640557f4e112128c223
marina.alvarezh@alumnos.upm.es <marina.alvarezh@alumnos.upm.es>
2022-05-20 13:54:36 +0200

    movimiento valido

--- Jaque-bot/Pieza.h
+++ Jaque-bot/Pieza.h
@@ -7,6 +7,7 @@ public:
 	Pieza(char c = '0', char t = '0') : color(c), tipo(t) {};
 	//virtual ~Pieza();
 	virtual void mueve() = 0;
+	virtual bool movimientoValido(V2D origen, V2D destino) = 0;
 	char getColor();
 	char getTipo();
 	V2D pos;
