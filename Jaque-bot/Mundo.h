444db9221ab6082428ff646913715e0de29a756f
jorgealmg <j.almoyna@alumnos.upm.es>
2022-05-21 15:34:28 +0200

    correción Mundo.h

--- Jaque-bot/Mundo.h
+++ Jaque-bot/Mundo.h
@@ -1,47 +1,25 @@
-#include "Mundo.h"
+#pragma once
 #include "freeglut.h"
 #include <math.h>
 
 
-void Mundo::rotarOjo()
+class Mundo 
 {
-	float dist = sqrt(x_ojo * x_ojo + z_ojo * z_ojo);
-	float ang = atan2(z_ojo, x_ojo);
-	ang += 0.5f;
-	x_ojo = dist * cos(ang);
-	z_ojo = dist * sin(ang);
-}
+public:
+	void rotarOjo();
 
-void Mundo::inicializa()
-{
+	void inicializa();
 
-}
+	void dibuja();
 
-void Mundo::dibuja()
-{
-	gluLookAt(x_ojo, y_ojo, z_ojo,
-		0.0, 0, 0.0,
-		0.0, 10.0, 0.0);
-}
+	void teclaEspecial(unsigned char key);
 
-void Mundo::teclaEspecial(unsigned char key)
-{
+	void mueve();
 
-}
+	Mundo();
 
-void Mundo::mueve()
-{
+	void tecla(unsigned char key);
+private:
+	int x_ojo, y_ojo, z_ojo;
 
-}
-
-Mundo::Mundo()
-{
-	x_ojo = 0;
-	y_ojo = 0;
-	z_ojo = 15;
-}
-
-void Mundo::tecla(unsigned char key)
-{
-
-}
+};
