480ce59cda7c06b9b3890d74723f05d729f617cd
jorgealmg <j.almoyna@alumnos.upm.es>
2022-05-22 12:51:33 +0200

    orientación correcta

--- Jaque-bot/Coordinador.cpp
+++ Jaque-bot/Coordinador.cpp
@@ -55,10 +55,10 @@ void Coordinador::dibuja()
 		glDisable(GL_LIGHTING);
 		glBegin(GL_POLYGON);
 		glColor3f(1, 1, 1);
-		glTexCoord2d(0, 0); glVertex3f(-5, -5, 0);
-		glTexCoord2d(1, 0); glVertex3f(-5, 5, 0);
-		glTexCoord2d(1, 1); glVertex3f(5, 5, 0);
-		glTexCoord2d(0, 1); glVertex3f(5, -5, 0);
+		glTexCoord2d(0, 0); glVertex3f(-5, 5, 0);
+		glTexCoord2d(1, 0); glVertex3f(5, 5, 0);
+		glTexCoord2d(1, 1); glVertex3f(5, -5, 0);
+		glTexCoord2d(0, 1); glVertex3f(-5, -5, 0);
 		glEnd();
 		glEnable(GL_LIGHTING);
 		glDisable(GL_TEXTURE_2D);
