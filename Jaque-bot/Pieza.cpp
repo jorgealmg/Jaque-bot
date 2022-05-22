4208f907e9870618168b1336eb8011d55c8f367c
marina.alvarezh@alumnos.upm.es <marina.alvarezh@alumnos.upm.es>
2022-05-22 17:55:40 +0200

    cambios comer peon

--- Jaque-bot/Pieza.cpp
+++ Jaque-bot/Pieza.cpp
@@ -16,12 +16,18 @@ bool Pieza::movimientoValido(V2D origen, V2D destino)
 	return false;
 }
 
-bool Pieza::comerPeon(V2D origen, V2D destino) {
+bool Pieza::comerPeon(V2D origen, V2D destino, Pieza *p) {
 	V2D res = destino - origen;
-
-
-	if (res.y == 1 && abs(res.x) == 1)
-		return true;
-	else
-		return false;
+	if (p->getColor() == B) {
+		if (res.y == 1 && abs(res.x) == 1)
+			return true;
+		else
+			return false;
+	}
+	if (p->getColor() == N) {
+		if (res.y == -1 && abs(res.x) == 1)
+			return true;
+		else
+			return false;
+	}
 }
